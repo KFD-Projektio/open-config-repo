3ac232331d539910be030ff099d46c720cfe4ab2
Michael Ostanin <ostaninmiha7@gmail.com>
2025-05-05 21:11:33 +0300

    added task service

--- postgres-init/init.sql
+++ postgres-init/init.sql
@@ -1,4 +1,11 @@
 CREATE DATABASE user_service_db;
+
 CREATE DATABASE board_service_db;
+
+CREATE DATABASE task_service_db;
+
 GRANT ALL PRIVILEGES ON DATABASE user_service_db TO admin;
+
 GRANT ALL PRIVILEGES ON DATABASE board_service_db TO admin;
+
+GRANT ALL PRIVILEGES ON DATABASE task_service_db TO admin;
